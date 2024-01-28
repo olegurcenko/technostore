dc9204186c083d7831ba4a21227a9cf6037585dc
olegurcenko <olegurcenko4@gmail.com>
2024-01-29 00:25:39 +0100

    -h

--- back/index.js
+++ back/index.js
@@ -35,7 +35,10 @@ app.use(express.json())
 
 const upload = multer({storage})
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    credentials: true,
+}));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Adjust the domain

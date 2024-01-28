73328d1775d8a236698c0ecafa8a00d111b1a1c6
olegurcenko <olegurcenko4@gmail.com>
2024-01-29 00:14:55 +0100

    -h

--- front/src/axios.js
+++ front/src/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'https://technostoreproj.com/api'
+	//baseURL: 'https://technostoreproj.com/api'
 	//baseURL: 'http://127.0.0.1:4444/api'
+	baseURL: 'https://technostore-p37h.vercel.app/api'
 });
 
 instance.interceptors.request.use((config) => {

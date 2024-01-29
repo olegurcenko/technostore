e784b63a50cddea9c84df17a759afb08b2821442
olegurcenko <olegurcenko4@gmail.com>
2024-01-29 12:08:46 +0100

    -h

--- back/index.js
+++ back/index.js
@@ -12,6 +12,12 @@ import { adminValidator } from './validations/adminDataValidator.js'
 mongoose.connect(
     'mongodb+srv://admin:admin12345@cluster0.damqkmb.mongodb.net/?retryWrites=true&w=majority'
 )
+.then(() => {
+    console.log('db is connected')
+})
+.catch((error) => {
+    console.log('db error: ', error)
+})
 
 const storage = multer.diskStorage({
     destination: (_, __, cb) => {
@@ -20,68 +26,105 @@ const storage = multer.diskStorage({
     filename: (_, file, cb) => {
         cb(null, file.originalname);
     },
-});
+})
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(express.json())
 
-app.use(express.json());
+const stripeInstance = stripe('sk_test_51Ocx0qLYGgmXoVpsC1mGKdd1pkI658RJaquQpcRx0ejLhvdFsBILKEtn8TC3yCsJjWaDZHKXGFjAh61ipykQBrWB00Crao9eeg');
 
-const upload = multer({ storage });
+const upload = multer({storage})
 
-app.use('/api/uploads', express.static('uploads'));
+app.use(cors())
+
+app.use('/uploads', express.static('uploads'))
 
 //! user requests
 
-app.post('/.netlify/functions/register', userValidator, errorsHandler, userController.register);
+app.post('/auth/register', userValidator, errorsHandler, userController.register)
 
-app.post('/.netlify/functions/login', userValidator, errorsHandler, userController.login);
+app.post('/auth/login', userValidator, errorsHandler, userController.login)
 
-app.post('/.netlify/functions/makeorder', userController.createOrder);
+app.post('/product/makeorder', userController.createOrder)
 
-app.get('/.netlify/functions/orders', userController.getOrders);
+app.get('/orders', userController.getOrders)
 
-app.get('/.netlify/functions/me', userAuth, userController.getMe);
+app.get('/auth/me', userAuth, userController.getMe)
 
 //! admin requests
 
-app.get('/.netlify/functions/admin/users', errorsHandler, adminController.getUsers);
+app.get('/admin/users', errorsHandler, adminController.getUsers)
 
-app.post('/.netlify/functions/adminregister', adminValidator, errorsHandler, adminController.register);
+app.post('/auth/adminregister', adminValidator, errorsHandler, adminController.register)
 
-app.post('/.netlify/functions/adminlogin', adminValidator, errorsHandler, adminController.login);
+app.post('/auth/adminlogin', adminValidator, errorsHandler, adminController.login)
 
-app.get('/.netlify/functions/admin/me', adminAuth, adminController.getMe);
+app.get('/auth/admin/me', adminAuth, adminController.getMe)
 
-app.get('/.netlify/functions/admin/orders', adminAuth, adminController.getOrders);
+app.get('/admin/orders', adminAuth, adminController.getOrders)
 
 //! products requests
 
-app.get('/.netlify/functions/product', productController.getAll);
+app.get('/product', productController.getAll)
 
-app.get('/.netlify/functions/product/search/:type', productController.getByType);
+app.get('/product/search/:type', productController.getByType)
 
-app.get('/.netlify/functions/product/search/title/:title', productController.getByTitle);
+app.get('/product/search/title/:title', productController.getByTitle)
 
-app.get('/.netlify/functions/product/:id', productController.getOne);
+app.get('/product/:id', productController.getOne)
 
-app.post('/.netlify/functions/product/add/:id', userController.addItem);
+app.post('/product/add/:id', userController.addItem)
 
-app.post('/.netlify/functions/product/remove/:id', userController.removeItem);
+app.post('/product/remove/:id', userController.removeItem)
 
-app.post('/.netlify/functions/product', adminAuth, productController.create);
+app.post('/product', adminAuth, productController.create)
 
-app.post('/.netlify/functions/product/:id', adminAuth, productController.remove);
+app.post('/product/:id', adminAuth, productController.remove)
 
-app.post('/.netlify/functions/product/update/:id', adminAuth, productController.update);
+app.post('/product/update/:id', adminAuth, productController.update)
 
-//!upload, not display
-
-app.post('/.netlify/functions/upload', upload.single('images'), (req, res) => {
+app.post('/upload', upload.single('images'), (req, res) => {
     res.json({
-        url: `/api/uploads/${req.file.originalname}`,
-    });
-});
+        url: `/uploads/${req.file.originalname}`,
+    })
+})
 
-module.exports = app;
+//! checkout request
+
+//app.post('/create-checkout-session', async (req, res) => {
+//    const { orderId, price } = req.body;
+  
+//    if (!orderId || !price) {
+//      return res.status(400).json({ error: 'orderId and price are required in the request body.' });
+//    }
+  
+//    try {
+//      const session = await stripeInstance.checkout.sessions.create({
+//        line_items: [
+//          {
+//            price: price,
+//            quantity: 1,
+//          },
+//        ],
+//        mode: 'payment',
+//        success_url: `${YOUR_DOMAIN}?success=true`,
+//        cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+//        client_reference_id: orderId, // Adding the orderId as a client reference ID
+//      });
+  
+//      res.redirect(303, session.url);
+//    } catch (error) {
+//      console.error('Error creating checkout session:', error);
+//      res.status(500).json({ error: 'Internal Server Error' });
+//    }
+//  });
+
+//!
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err)
+    }
+    console.log('server ok')
+})
--- front/src/axios.js
+++ front/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
 	//baseURL: 'https://technostoreproj.com/api'
-	//baseURL: 'http://localhost:4444/api'
+	//baseURL: 'http://localhost:4444'
 	//baseURL: 'https://technostore-p37h.vercel.app/api'
 	baseURL: 'http://37.1.218.188:4444'
 });
